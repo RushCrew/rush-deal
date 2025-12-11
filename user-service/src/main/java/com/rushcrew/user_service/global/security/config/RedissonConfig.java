64f98da57d761d62e8ff8c4f29a775cca9588605
bbangjae <thebyunjae@naver.com>
2025-12-11 12:13:55 +0900

    feat(user): QueryDSL 및 Redisson 설정 추가

---
+++ user-service/src/main/java/com/rushcrew/user_service/global/security/config/RedissonConfig.java
@@ -0,0 +1,37 @@
+package com.rushcrew.user_service.global.security.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+
+        config.useSingleServer()
+            .setAddress("redis://" + redisHost + ":" + redisPort)
+            .setPassword(redisPassword)
+            .setConnectionPoolSize(50)
+            .setConnectionMinimumIdleSize(10)
+            .setRetryAttempts(3)
+            .setRetryInterval(1500);
+
+        return Redisson.create(config);
+    }
+}
