10f18ab11005c3df3b1e0622b01a00ee092c48ea
songkyung <min.songkyung@gmail.com>
2025-12-22 18:35:22 +0900

    refactor: SecurityConfig actuator 헬스 체크 허용 처리

--- payment-service/src/main/java/com/rushcrew/payment_service/global/security/config/SecurityConfig.java
+++ payment-service/src/main/java/com/rushcrew/payment_service/global/security/config/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
-
-
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
@@ -19,7 +17,13 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+                .authorizeHttpRequests(auth -> auth
+                    // 경로별 권한 설정
+                    // 보통 @PreAuthorize로 처리하므로 모두 허용하거나 authenticated로 설정
+                    // Actuator나 헬스 체크 경로는 열어두는 것이 좋음
+                    .requestMatchers("/actuator/**").permitAll()
+                    .anyRequest().authenticated()
+                );
         return http.build();
     }
 }
