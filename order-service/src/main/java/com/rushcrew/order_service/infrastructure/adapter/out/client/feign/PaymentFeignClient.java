9c69d3a79f3ce247330245febfd397936f24be65
songkyung <min.songkyung@gmail.com>
2025-12-23 01:53:04 +0900

    refactor: user-service eureka 설정 false

--- order-service/src/main/java/com/rushcrew/order_service/infrastructure/adapter/out/client/feign/PaymentFeignClient.java
+++ order-service/src/main/java/com/rushcrew/order_service/infrastructure/adapter/out/client/feign/PaymentFeignClient.java
@@ -8,7 +8,7 @@ import com.rushcrew.order_service.infrastructure.dto.payment.PaymentCancelReques
 import com.rushcrew.order_service.infrastructure.dto.payment.PaymentPrepareResponse;
 import com.rushcrew.order_service.infrastructure.dto.payment.PaymentRequest;
 
-@FeignClient(name = "payment-service")
+@FeignClient(name = "payment-service", url = "${PAYMENT_SERVICE_URL}")
 public interface PaymentFeignClient {
 	@PostMapping("/api/v1/payments")
 	PaymentPrepareResponse requestPayment(@RequestBody PaymentRequest request);
