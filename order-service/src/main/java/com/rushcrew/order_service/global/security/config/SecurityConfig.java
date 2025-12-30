f732eaa0468f0143c7db7da6fe42497b4edc4d74
songkyung <min.songkyung@gmail.com>
2025-12-30 17:50:38 +0900

    fix: order-service application-prod, securityconfig 수정

--- order-service/src/main/java/com/rushcrew/order_service/global/security/config/SecurityConfig.java
+++ order-service/src/main/java/com/rushcrew/order_service/global/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.rushcrew.order_service.global.security.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -22,10 +23,14 @@ public class SecurityConfig {
             .sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용 안 함 (Stateless 설정)
             .authorizeHttpRequests(auth -> auth
+                // Actuator 엔드포인트를 경로와 상관없이 모두 허용
+                // 엔드포인트 요청(health, info 등)을 자동으로 인식하여 허용합니다.
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
                 // 경로별 권한 설정
                 // 보통 @PreAuthorize로 처리하므로 모두 허용하거나 authenticated로 설정
                 // Actuator나 헬스 체크 경로는 열어두는 것이 좋음
-                .requestMatchers("/actuator/**").permitAll()
+                .requestMatchers("/actuator/**", "/health").permitAll()
+                .requestMatchers("/api/v1/orders/**", "/api/v1/batch/**").permitAll()
                 .anyRequest().authenticated()
             );
         return http.build();
