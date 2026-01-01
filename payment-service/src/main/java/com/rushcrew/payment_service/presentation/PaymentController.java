0aff87f38975a90ac384ed91a8d6661d66635945
songkyung <min.songkyung@gmail.com>
2026-01-02 00:49:15 +0900

    fix: user-service SecurityConfig, application yml 수정

--- payment-service/src/main/java/com/rushcrew/payment_service/presentation/PaymentController.java
+++ payment-service/src/main/java/com/rushcrew/payment_service/presentation/PaymentController.java
@@ -12,6 +12,7 @@ import com.rushcrew.payment_service.presentation.dto.response.PaymentResponse;
 import jakarta.validation.Valid;
 import kotlin.Unit;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/payments")
 @RequiredArgsConstructor
