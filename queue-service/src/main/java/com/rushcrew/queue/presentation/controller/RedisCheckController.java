528e3026ab01b0db7b0e8214c14f6df43cec5ac2
songkyung <min.songkyung@gmail.com>
2026-01-05 02:49:27 +0900

    chore: redis 전체 초기화용 엔드포인트 생성 및 aws 서비스 중지 sh 생성

--- queue-service/src/main/java/com/rushcrew/queue/presentation/controller/RedisCheckController.java
+++ queue-service/src/main/java/com/rushcrew/queue/presentation/controller/RedisCheckController.java
@@ -1,7 +1,10 @@
 package com.rushcrew.queue.presentation.controller;
 
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,5 +19,12 @@ public class RedisCheckController {
     @GetMapping("/redis/check/{key}")
     public String getValue(@PathVariable String key) {
         return "Value: " + redisTemplate.opsForValue().get(key);
+    }
+
+    @DeleteMapping("/redis/clear")
+    public ResponseEntity<String> clearRedis() {
+        // RedisTemplate 사용
+        Objects.requireNonNull(redisTemplate.getConnectionFactory()).getConnection().flushAll();
+        return ResponseEntity.ok("Redis Cleaned!");
     }
 }
